ba4ea988de07b312a1368aca199bb2ce1183ec2e
Mark-Timi <1466034796@qq.com>
2024-10-13 23:32:57 +0800

    update

--- app/layout.js
+++ app/layout.js
@@ -25,8 +25,11 @@ export default function RootLayout({ children }) {
 
   const footer = (
     <footer className="p-4 sm:p-8 grid place-items-center">
-      <Link href={'https://www.baidu.com/'} target="_blank" className="">
-        <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  ' + fugaz.className}>Built by Smoljames 💛</p>
+      <Link href={'https://nfthomeland.github.io/'} target="_blank" className="">
+        <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  ' + fugaz.className}>NFT 😍</p>
+      </Link>
+      <Link href={'https://mouthAma.github.io/'} target="_blank" className="">
+        <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  ' + fugaz.className}>Mouth🤩</p>
       </Link>
     </footer>
   )
